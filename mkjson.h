8853fefe9c817d93f06450689f1832905226a61b
Pegah-source <66211061+Pegah-source@users.noreply.github.com>
2021-02-24 14:36:46 -0500

    Create mkjson.h

---
+++ mkjson.h
@@ -0,0 +1,13 @@
+#ifndef URL_H
+#define URL_H
+
+typedef struct inf
+{
+	//the array of size 64 of words
+	char* nextWords[64];
+	//we must also keep the current size of this array
+	int size;//must be initialized to 0
+} inf ;
+
+
+#endif
