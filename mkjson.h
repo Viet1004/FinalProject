0f70ff31c3e07526f91d4efeaa43c7f2002ad490
Pegah-source <66211061+Pegah-source@users.noreply.github.com>
2021-02-26 03:57:48 -0500

    Update mkjson.h

--- mkjson.h
+++ mkjson.h
@@ -4,10 +4,11 @@
 typedef struct inf
 {
 	//the array of size 64 of words
-	char* nextWords[64];
+	char nextWords[64][40];
 	//we must also keep the current size of this array
 	int size;//must be initialized to 0
 } inf ;
-
+void complete(char words[2000][40],inf* pointersToInf,int countext);
+void createJson(char words[2000][40],inf* pointersToInf,int countext);
 
 #endif
