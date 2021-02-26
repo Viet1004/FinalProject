72eb21d52472b6b2f36e6427758ef5e08b05c75f
Viet <nguyenquocviet1004@gmail.com>
2021-02-26 13:14:43 +0100

    Tweet

--- postTweet.py
+++ postTweet.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'TY9EAjgje5fP2cY1msdbLMF1T'
-consumer_secret = 'wzR72efd3V5g5bMN9t7FO3l6lhwWya5m2HxWeOV2Mlftj15ETt'
-access_token = '901352542470152192-bgPb0kT0uha8DaqnbZAQaJ0NNhCXgEd'
-access_token_secret = '4LNlU1H80byXmL8uNBGNBl7EtG3GMqTRO9MwORtslSE0o'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 def OAuth():
 	try:
